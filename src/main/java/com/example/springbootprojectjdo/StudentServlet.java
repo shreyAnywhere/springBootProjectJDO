98f7751d1dde1d54c470ada1e69821660607e2eb
Shreypatel911 <shreypatel11999@gmail.com>
2022-06-03 18:48:19 +0530

    first commit

--- src/main/java/com/example/springbootprojectjdo/StudentServlet.java
+++ src/main/java/com/example/springbootprojectjdo/StudentServlet.java
@@ -1,18 +1,11 @@
 package com.example.springbootprojectjdo;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.apphosting.api.DatastorePb;
 import org.datanucleus.api.jdo.JDOPersistenceManagerFactory;
 import org.datanucleus.metadata.PersistenceUnitMetaData;
-
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Transaction;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
