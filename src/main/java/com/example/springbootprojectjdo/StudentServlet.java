94a7ea3e76f0d55bc08cb57984c63a5cf28d97da
Shreypatel911 <shreypatel11999@gmail.com>
2022-06-03 15:49:27 +0530

    first commit

--- src/main/java/com/example/springbootprojectjdo/StudentServlet.java
+++ src/main/java/com/example/springbootprojectjdo/StudentServlet.java
@@ -1,7 +1,13 @@
 package com.example.springbootprojectjdo;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.apphosting.api.DatastorePb;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +17,13 @@ public class StudentServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        Entity e = new Entity("Student");
+        e.setProperty("name", "abc");
+        e.setProperty("email", "abc@gmail.com");
+        e.setProperty("age", 30);
+        ds.put(e);
 
         resp.getWriter().println("Hello world from Student servlet!!!");
     }
