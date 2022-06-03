e938cbd6fd22431f73eb57b4251060bf23e5fa3c
Shreypatel911 <shreypatel11999@gmail.com>
2022-06-03 16:55:46 +0530

    first commit

--- src/main/java/com/example/springbootprojectjdo/StudentDetails.java
+++ src/main/java/com/example/springbootprojectjdo/StudentDetails.java
@@ -1,19 +1,32 @@
 package com.example.springbootprojectjdo;
 
+import com.google.appengine.api.datastore.Key;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 import java.time.LocalDate;
 
+@PersistenceCapable
 public class StudentDetails {
-    private long ID;
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    private Key ID;
+    @Persistent
     private String name;
+    @Persistent
     private String email;
+    @Persistent
     private LocalDate dob;
+    @Persistent
     private int age;
 
-    public long getID() {
+    public Key getID() {
         return ID;
     }
 
-    public void setID(long ID) {
+    public void setID(Key ID) {
         this.ID = ID;
     }
 
--- src/main/java/com/example/springbootprojectjdo/StudentServlet.java
+++ src/main/java/com/example/springbootprojectjdo/StudentServlet.java
@@ -5,6 +5,8 @@ import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.apphosting.api.DatastorePb;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,18 +14,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class StudentServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-        Entity e = new Entity("Student");
-        e.setProperty("name", "abc");
-        e.setProperty("email", "abc@gmail.com");
-        e.setProperty("age", 30);
-        ds.put(e);
+//        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+//        Entity e = new Entity("Student");
+//        e.setProperty("name", "abc");
+//        e.setProperty("email", "abc@gmail.com");
+//        e.setProperty("age", 30);
+//        ds.put(e);
+        try{
+            StudentDetails object = new StudentDetails();
+            object.setName("abc");
+            object.setEmail("abc@gmail.com");
+            object.setAge(30);
+            object.setDob(LocalDate.parse("2021-01-07"));
+            PersistenceManager persistenceManager = null;
+            persistenceManager.makePersistent(object);
+        } catch (Exception e) {
+            System.out.print(e.getMessage());
+        }
 
         resp.getWriter().println("Hello world from Student servlet!!!");
     }
